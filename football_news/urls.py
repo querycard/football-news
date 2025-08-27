b9902606901979505135bb6f8e01bbcfa58cf3a2
querycard <khansa.dinda@ui.ac.id>
2025-08-27 17:38:29 +0700

    tutorial 0

--- football_news/urls.py
+++ football_news/urls.py
@@ -13,8 +13,9 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+halo
 """
-#cfcfcgh
 
 
 from django.contrib import admin
